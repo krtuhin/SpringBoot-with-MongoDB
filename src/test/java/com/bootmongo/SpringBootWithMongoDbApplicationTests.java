6054b2219cea70472b6ee319411f733a44cb5c29
krtuhin <tuhin.km2774@gmail.com>
2023-08-26 03:55:11 +0530

    Project Created

---
+++ src/test/java/com/bootmongo/SpringBootWithMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
